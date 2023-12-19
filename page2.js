66e783111d8eded5adf21cf2fd5959e283a5c56a
m-dev-39 <151646565+m-dev-39@users.noreply.github.com>
2023-12-19 18:10:22 +0530

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,65 +1,64 @@
-resultEl = document.getElementById('result')
-const lengthEl = document.getElementById('length')
-const uppercaseEl = document.getElementById('uppercase')
-const lowercaseEl = document.getElementById('lowercase')
-const numbersEl = document.getElementById('numbers')
-const symbolsEl = document.getElementById('symbols')
-const generateEl = document.getElementById('generate')
-const clipboardEl = document.getElementById('clipboard')
+document.getElementById('generate').addEventListener('click', generatePassword);
+
+function generatePassword() {
+    const length = document.getElementById('length').value;
+    const hasLower = document.getElementById('lowercase').checked;
+    const hasUpper = document.getElementById('uppercase').checked;
+    const hasNumber = document.getElementById('numbers').checked;
+    const hasSymbol = document.getElementById('symbols').checked;
+
+    const result = document.getElementById('result');
+    result.value = generatePasswordString(length, hasLower, hasUpper, hasNumber, hasSymbol);
+}
+
+function generatePasswordString(length, lower, upper, number, symbol) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+
+    if (typesCount === 0) {
+        return '';
+    }
+
+    for (let i = 0; i < length; i += typesCount) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+
+    return generatedPassword.slice(0, length);
+}
+
+function copyToClipboard() {
+    const result = document.getElementById('result');
+    if (result.value) {
+        result.select();
+        document.execCommand('copy');
+        alert('Password copied to clipboard!');
+    }
+}
+
 const randomFunc = {
-lower: getRandomLower,
-upper: getRandomUpper,
-number: getRandomNumber,
-symbol: getRandomSymbol
-}
-clipboardEl.addEventListener('click', () => {
-const textarea = document.createElement('textarea')
-const password = resultEl.innerText
-if(!password) { return }
-textarea.value = password
-document.body.appendChild(textarea)
-textarea.select()
-document.execCommand('copy')
-textarea.remove()
-alert('Password copied to clipboard!')
-})
-generateEl.addEventListener('click', () => {
-const length = +lengthEl.value
-const hasLower = lowercaseEl.checked
-const hasUpper = uppercaseEl.checked
-const hasNumber = numbersEl.checked
-const hasSymbol = symbolsEl.checked
-resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol,
-length)
-})
-function generatePassword(lower, upper, number, symbol, length) {
-let generatedPassword = ''
-const typesCount = lower + upper + number + symbol
-27
-const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item =>
-Object.values(item)[0])
-if(typesCount === 0) {
-return ''
-}
-for(let i = 0; i < length; i += typesCount) {
-typesArr.forEach(type => {
-const funcName = Object.keys(type)[0]
-generatedPassword += randomFunc[funcName]()
-})
-}
-const finalPassword = generatedPassword.slice(0, length)
-return finalPassword
-}
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
 function getRandomLower() {
-return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
+
 function getRandomUpper() {
-return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
+
 function getRandomNumber() {
-return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
+
 function getRandomSymbol() {
-const symbols = '!@#$%^&*(){}[]=<>/,.'
-return symbols[Math.floor(Math.random() * symbols.length)]
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
 }
